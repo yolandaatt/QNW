28a628640c4e518ebb52542477cc9f26dab3b43d
yolandaatt <yolandaatteyeh3@gmail.com>
2026-05-28 08:41:43 +0200

    fix: CORS origin

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,6 +19,10 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [];
 app.use(
   cors({
     origin: (origin, callback) => {
+      if (allowedOrigins.includes("*")) {
+        callback(null, true);
+        return;
+      }
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
